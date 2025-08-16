5f882b8840a64ddba29b6b26c0a42ba57190f2d9
Christian Souza <christian.s.bazilio@hotmail.com>
2025-08-16 00:54:55 -0300

    ex 008, 009 e 010

---
+++ Exercícios/ex008.js
@@ -0,0 +1,8 @@
+var vel = 61
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+console.log(`Dirija sempre com o cinto de segurança!`)
+
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade máxima permitida, você foi multado!`)
+}
