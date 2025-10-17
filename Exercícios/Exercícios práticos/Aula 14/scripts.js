b61fa9d10a77db109369e7b258a2f427d74d0aad
Christian Souza <christian.s.bazilio@hotmail.com>
2025-10-16 23:36:20 -0300

    Att

--- Exercícios/Exercícios práticos/Aula 14/scripts.js
+++ Exercícios/Exercícios práticos/Aula 14/scripts.js
@@ -1,13 +1,14 @@
-var inicio = document.getElementById('inicio')
-var fim = document.getElementById('fim')
-var passo = document.getElementById('passo')
-var res = document.getElementById('res')
+let inicio = document.body.getElementById('inicio')
+let fim = document.getElementById('fim')
+let passo = document.getElementById('passo')
+let res = document.getElementById('res')
 
-var n1 = Number(inicio.value)
-var n2 = Number(fim.value)
-var n3 = Number(passo.value)
+let n1 = Number(inicio.value)
+let n2 = Number(fim.value)
+let n3 = Number(passo.value)
 
 function contar(){
     window.alert('Olá')
     res.innerHTML = `${n1}, ${n2}, ${n3}`
 }
+
