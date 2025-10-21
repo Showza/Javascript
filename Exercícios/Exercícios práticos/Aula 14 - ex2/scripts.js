d7b05b636f30fd0cc30b9a7fa88ccc9e87c5a646
Christian Souza <christian.s.bazilio@hotmail.com>
2025-10-21 13:16:15 -0300

    Ex2

--- Exercícios/Exercícios práticos/Aula 14 - ex2/scripts.js
+++ Exercícios/Exercícios práticos/Aula 14 - ex2/scripts.js
@@ -1,51 +1,16 @@
-function verificar(){
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
+function tabuada(){
+    let numero = document.getElementById('txtn')
+    let n = Number(numero.value)
+    let tabuada = document.getElementById('seltab')
 
-    if(fano.value.length == 0 || fano.value > ano){
-        window.alert('Erro! Verifique o ano de nascimento inserido e tente novamente.')
-    } else{
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - fano.value
-        var genero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-
-        if(fsex[0].checked){
-            genero = 'Homem'
-            if(idade >= 0 && idade <= 10){
-                //Criança
-                img.setAttribute('src', 'imgs/criancah.jpg')
-            } else if (idade > 10 && idade <= 21){
-                //Jovem
-                img.setAttribute('src', 'imgs/jovemh.jpg')
-            } else if(idade > 21 && idade <= 50){
-                //Adulto
-                img.setAttribute('src', 'imgs/adultoh.jpg')
-            } else{
-                //Idoso
-                img.setAttribute('src', 'imgs/velho.jpg')
-            }
-        } else{
-            genero = 'Mulher'
-            if(idade >= 0 && idade <= 10){
-                //Criança
-                img.setAttribute('src', 'imgs/criancam.jpg')
-            } else if (idade > 10 && idade <= 21){
-                //Jovem
-                img.setAttribute('src', 'imgs/jovemm.jpg')
-            } else if(idade > 21 && idade <= 50){
-                //Adulto
-                img.setAttribute('src', 'imgs/adultom.jpg')
-            } else{
-                //Idoso
-                img.setAttribute('src', 'imgs/velha.jpg')
-            }
-        }
-        
-        res.innerHTML = `Você é um(a) ${genero} com ${idade} anos.`
-        res.appendChild(img)
+    if (n <= 0){
+        window.alert('Erro! O número precisa ser maior ou igual a 1.')
+    } else {
+        let c = 1
+        do{
+            let tab = n * c
+            tabuada.innerHTML += `${tab}`
+            c++
+        } while (c < 10)
     }
 }
